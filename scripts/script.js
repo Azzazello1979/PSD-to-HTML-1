fc27e87cf4ea0a100f59ceb139be7c1ae4041942
Balint H <balint.haui@gmail.com>
2020-01-21 19:37:18 +0100

    nice

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    
+    // $("h3").css("border", "1px solid blue"); // for applying one css prop.
+    // $("#lead-banner").css({ border: "1px solid green", width: "100px" }); // for applying multiple css props. in an object
+
+    // FILTERS ... refining selection
+
+    // firs & last occurance
+    // $("header nav li:first").css({ border: "2px solid red" });
+    // $("header nav li:last").css({ border: "2px solid red" });
+
+    // first-child & last-child
+    // $("#contact ul:first-child").css({ border: "2px solid red" });
+    // $("#contact ul:last-child").css({ border: "2px solid red" });
+
+    //$("#contact img").css({ border: "2px solid red" });
+
+    // even
+    // $("header nav li:even").css({ border: "2px solid blue" });
+    // $("header nav li:odd").css({ border: "2px solid pink" });
+
+    
+
+
+})
