8e6b1b06a3de739fda79a398bed0f2bf6aca8bf7
Balint H <balint.haui@gmail.com>
2020-01-21 20:13:46 +0100

    juj

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
     // $("h3").css("border", "1px solid blue"); // for applying one css prop.
     // $("#lead-banner").css({ border: "1px solid green", width: "100px" }); // for applying multiple css props. in an object
 
-    // FILTERS ... refining selection
+    ////////////////////////////////////
+    // FILTERS ... refining selection //
+    ////////////////////////////////////
 
     // firs & last occurance
     // $("header nav li:first").css({ border: "2px solid red" });
@@ -15,11 +17,34 @@ $(document).ready(function(){
 
     //$("#contact img").css({ border: "2px solid red" });
 
-    // even
+    // even & odd
     // $("header nav li:even").css({ border: "2px solid blue" });
     // $("header nav li:odd").css({ border: "2px solid pink" });
 
-    
+    // not
+    // $("section:not('#clients')").css({ border: "2px dotted orange" });
 
+    // less than (lt)
+    // $("#social-nav li:lt(3)").css({ border: "2px dotted red" });
+
+    // greater than (gt)
+    // $("#social-nav li:gt(2)").css({ border: "2px dotted red" });
+
+    // ATTRIBUTE FILTERS
+    // go after every div that has a class..
+    // $("div[class]").css({ border: "2px dotted green" });
+
+    // go after every image that has alt..
+    // $("img[alt]").css({ border: "2px dotted red" });
+
+    // ATTRIBUTE FILTERS WITH SPEC. VALUES
+
+    // go after every list-item that has a class "facebook"
+    // $("li[class=facebook]").css({ border: "2px dotted green" });
+
+    // go after every image that has alt "quote"
+    //$("img[alt=quote]").css({ border: "2px dotted red" });
+
+    
 
 })
